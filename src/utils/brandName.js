3aeb60f6b7f7edfe385c2c7e108235bb33536911
palakgrewox <palakgrewox@gmail.com>
2025-08-29 11:09:48 +0530

    Update BrandConfig to reflect new branding: Changed appCapitalName and appSmallName to Grewox and grewox respectively, while commenting out the previous Raiser branding for clarity.

--- src/utils/brandName.js
+++ src/utils/brandName.js
@@ -1,9 +1,9 @@
 class BrandConfig {
-    static appCapitalName = "Raiser"; // Updated brand name
-    static appSmallName = "raiser"; 
+    // static appCapitalName = "Raiser"; // Updated brand name
+    // static appSmallName = "raiser"; 
     
-    // static appCapitalName = "Grewox";
-    // static appSmallName = "grewox"// Updated brand name
+    static appCapitalName = "Grewox";
+    static appSmallName = "grewox"// Updated brand name
 }
 
 export default BrandConfig;
